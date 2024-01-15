72142331f1d06d01a5e7d7b44eb7b5e6d55428ae
Tomisin <tomisinjoseph3@gmail.com>
2024-01-15 11:20:12 +0000

    Creating Scene component

---
+++ src/Scene.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Scene() {
+  return (
+    <div>
+      <p>Scene 1</p>
+      <div>
+        <img></img>
+        <img></img>
+        <img></img>
+      </div>
+      <div>
+        <p>description</p>
+      </div>
+      <div>
+        <p>tag 1</p>
+        <p>tag 2</p>
+      </div>
+    </div>
+  )
+}
+
+export default Scene
